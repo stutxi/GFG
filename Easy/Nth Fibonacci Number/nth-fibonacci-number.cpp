6301ebfa6e4edc0aecfdc735dd37514812eeaebf
Stuti Sharma <95741837+stutxi@users.noreply.github.com>
2024-02-10 23:26:05 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int nthFibonacci(int n){
+        // code here
+        const int MOD = 1000000007;
+        if (n <= 1)
+            return n;
+        
+        int fib[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        
+        for (int i = 2; i <= n; ++i) {
+            fib[i] = (fib[i - 1] + fib[i - 2]) % MOD;
+        }
+        return fib[n];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.nthFibonacci(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
