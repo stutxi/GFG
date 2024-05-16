ef3edbe0271ad10fab2321032893a19dff0b5a81
Stuti Sharma <95741837+stutxi@users.noreply.github.com>
2024-05-17 01:10:40 +0530

    Updated Solution - GfG to GitHub

--- Easy/Find Pair Given Difference/find-pair-given-difference.cpp
+++ Easy/Find Pair Given Difference/find-pair-given-difference.cpp
@@ -1,55 +1,78 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
- 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
-
-bool findPair(int arr[], int size, int n);
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int l,n;
-        cin>>l>>n;
-        int arr[l];
-        for(int i=0;i<l;i++)
-            cin>>arr[i];
-        if(findPair(arr, l, n))
-            cout<<1<<endl;
-        else cout<<"-1"<<endl;
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
     }
-    
-  
-    return 0;
-}
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
 // } Driver Code Ends
 
-bool findPair(int arr[], int size, int n){
-    //code
-    sort(arr, arr + size);
-
-    int i = 0;
-    int j = 1;
-
-    // Iterate through the array
-    while (i < size && j < size) {
-        int diff = arr[j] - arr[i];
-
-        if (diff == n) {
-            return true;
-        }
-        else if (diff < n) {
-            j++;
-        }
-        else {
-            i++;
-            if (i == j) {
-                j++;
+class Solution {
+  public:
+    int findPair(int n, int x, vector<int> &arr) {
+        // code here
+        sort(arr.begin(), arr.end());
+        
+        int left = 0;
+        int right = 1;
+        
+        while (right < n) {
+            int diff = abs(arr[right] - arr[left]);
+            
+            if (diff == x) {
+                return 1;
+            }
+            else if (diff < x) {
+                right++;
+            }
+            else {
+                left++;
+                if (left == right) {
+                    right++;
+                }
             }
         }
+        return -1;
     }
-    return false;
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        int x;
+        scanf("%d", &x);
+
+        vector<int> arr(n);
+        Array::input(arr, n);
+
+        Solution obj;
+        int res = obj.findPair(n, x, arr);
+
+        cout << res << endl;
+    }
 }
+
+// } Driver Code Ends
