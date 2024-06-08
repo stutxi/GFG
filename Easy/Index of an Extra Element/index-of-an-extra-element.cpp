4ad5412af7f667b15d95435c65b09778d5618482
Stuti Sharma <95741837+stutxi@users.noreply.github.com>
2024-06-08 22:45:01 +0530

    Updated Solution - GfG to GitHub

--- Easy/Index of an Extra Element/index-of-an-extra-element.cpp
+++ Easy/Index of an Extra Element/index-of-an-extra-element.cpp
@@ -8,17 +8,13 @@ class Solution {
   public:
     int findExtra(int n, int arr1[], int arr2[]) {
         // add code here.
-        int start = 0;
-        int end = n - 2;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (arr1[mid] == arr2[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        int i = 0;
+        while (i < n) {
+            if(arr1[i] != arr2[i]) {
+                return i;
             }
+            i++;
         }
-        return start;
     }
 };
 
