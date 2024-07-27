a6b1262da103d4d48af1e31f4d7f41143ac06583
Stuti Sharma <95741837+stutxi@users.noreply.github.com>
2024-07-27 23:46:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Identical Linked Lists/identical-linked-lists.cpp
+++ Difficulty: Basic/Identical Linked Lists/identical-linked-lists.cpp
@@ -66,14 +66,16 @@ struct Node {
 // Function to check whether two linked lists are identical or not.
 bool areIdentical(struct Node *head1, struct Node *head2) {
     // Code here
-    while (head1 != NULL && head2 != NULL) {
-        if (head1->data != head2->data) {
+    Node* temp1 = head1;
+    Node* temp2 = head2;
+    while (temp1 != nullptr && temp2 != nullptr) {
+        if (temp1->data != temp2->data) {
             return false;
         }
-        head1 = head1->next;
-        head2 = head2->next;
+        temp1 = temp1->next;
+        temp2 = temp2->next;
     }
-    if (head1 == NULL && head2 == NULL) {
+    if (temp1 == nullptr && temp2 == nullptr) {
         return true;
     }
     return false;
