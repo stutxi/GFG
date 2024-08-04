af6ad612a1852a55a5f8506790f0b040612edb34
Stuti Sharma <95741837+stutxi@users.noreply.github.com>
2024-08-04 18:45:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        // Your code here
+        stack<char> st;
+        for (char ch : x) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (!st.empty()) {
+                    //closing bracket
+                    if (ch == ')' && st.top() == '(' ) {
+                        st.pop();
+                    } else if (ch == '}' && st.top() == '{') {
+                        st.pop();
+                    } else if (ch == ']' && st.top() == '[') {
+                        st.pop();
+                    } else {
+                        return false;
+                    }   
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}
+// } Driver Code Ends
