e5bca78009a0a36de3d39371108ee5a6129dfb3c
Stuti Sharma <95741837+stutxi@users.noreply.github.com>
2024-08-07 23:03:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -8,15 +8,35 @@ class Solution {
   public:
     int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
         // code here
-        vector<int> arr;
-        for (auto num : arr1) {
-            arr.push_back(num);
+        int n = arr1.size();
+        int m = arr2.size();
+
+        // ensure arr1 is the smaller array
+        if (n > m) {
+            return kthElement(k, arr2, arr1);
         }
-        for (auto num : arr2) {
-            arr.push_back(num);
+
+        int low = max(0, k - m);
+        int high = min(k, n);
+
+        while (low <= high) {
+            int i1 = (low + high) / 2;
+            int i2 = k - i1;
+
+            int min1 = (i1 == n) ? INT_MAX : arr1[i1];
+            int max1 = (i1 == 0) ? INT_MIN : arr1[i1 - 1];
+            int min2 = (i2 == m) ? INT_MAX : arr2[i2];
+            int max2 = (i2 == 0) ? INT_MIN : arr2[i2 - 1];
+
+            if (max1 <= min2 && max2 <= min1) {
+                return max(max1, max2);
+            } else if (max1 > min2) {
+                high = i1 - 1;
+            } else {
+                low = i1 + 1;
+            }
         }
-        sort(arr.begin(), arr.end());
-        return arr[k - 1];
+        return -1; 
     }
 };
 
