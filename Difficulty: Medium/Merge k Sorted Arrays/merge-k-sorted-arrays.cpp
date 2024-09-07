5b9f415849a818048e0f7a4a317a6e7746712df3
Stuti Sharma <95741837+stutxi@users.noreply.github.com>
2024-09-07 22:41:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+#define N 105
+using namespace std;
+void printArray(vector<int> arr, int size)
+{
+for (int i=0; i < size; i++)
+	cout << arr[i] << " ";
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Info {
+    public: 
+    int data;
+    int rowIndex;
+    int colIndex;
+    Info(int data, int row, int col) : data(data), rowIndex(row), colIndex(col) {}
+};
+
+class compare {
+    public: 
+    bool operator()(Info* a, Info* b) {
+        return a->data > b->data;
+    }
+};
+
+class Solution
+{
+    public:
+    //Function to merge k sorted arrays.
+    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
+    {
+        //code here
+        priority_queue<Info*, vector<Info*>, compare> pq;
+        vector<int> ans;
+        
+        for (int row = 0; row < K; row++) {
+            Info* temp = new Info(arr[row][0], row, 0);
+            pq.push(temp);
+        }
+        
+        while (!pq.empty()) {
+            Info* temp = pq.top();
+            pq.pop();
+            
+            int topData = temp->data;
+            int topRow = temp->rowIndex;
+            int topCol = temp->colIndex;
+            
+            ans.push_back(topData);
+            
+            if (topCol + 1 < K) {
+                Info* newInfo = new Info(arr[topRow][topCol + 1], topRow, topCol + 1);
+                pq.push(newInfo);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+	    int k;
+	    cin>>k;
+	    vector<vector<int>> arr(k, vector<int> (k, 0));
+	    for(int i=0; i<k; i++){
+	        for(int j=0; j<k; j++)
+	        {
+	            cin>>arr[i][j];
+	        }
+	    }
+	    Solution obj;
+    	vector<int> output = obj.mergeKArrays(arr, k);
+    	printArray(output, k*k);
+    	cout<<endl;
+    }
+	return 0;
+}
+
+
+
+
+
+
+// } Driver Code Ends
